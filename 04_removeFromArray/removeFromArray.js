407dec75af74389235e2ff299ec9249a5dbb9341
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-24 14:35:30 -0600

    Write the code to pass the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array = []) {
+    let indexOfValue;
+    let i = 1;
+
+    for (; i < arguments.length; ) {
+        indexOfValue = array.indexOf(arguments[i]);
+        if(indexOfValue === -1) {
+            i++;
+        } else {
+            array.splice(indexOfValue, 1);
+        }
+    }
+    return array;
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

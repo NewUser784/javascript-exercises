110c42ad72b3fa4269cc14a150536f5d31aa0c81
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-24 16:28:11 -0600

    Modify code to also pass the 4th test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,9 @@
 const sumAll = function(num1 = 0, num2 = 0) {
     let sum = 0;
 
-    if (num1 < num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 < num2) {
         for (; num1 <= num2; num1++) {
             sum += num1;
         }

f324cb74defee69b28b65e9e92f71c17b39d198d
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-24 16:23:43 -0600

    Write the code that passes the tests
    This code only solves the first 2 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(num1 = 0, num2 = 0) {
+    let sum = 0;
 
+    for (; num1 <= num2; num1++) {
+        sum += num1;
+    }
+    return sum;
 };
 
 // Do not edit below this line

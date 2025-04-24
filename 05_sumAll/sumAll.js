e8595713a7054433a90f8709e09de9a89df9fd01
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-04-24 16:25:55 -0600

    Modify code to also solve the 3rd test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(num1 = 0, num2 = 0) {
     let sum = 0;
 
-    for (; num1 <= num2; num1++) {
-        sum += num1;
+    if (num1 < num2) {
+        for (; num1 <= num2; num1++) {
+            sum += num1;
+        }
+    } else {
+        for (; num2 <= num1; num2++) {
+            sum += num2;
+        }
     }
     return sum;
 };

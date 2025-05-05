726d2ec7ce32dc176e0117a26677c44bdf2b604d
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-05-05 17:43:35 -0600

    Modify code to take numbers into consideration
    Originally, the program removed all punctuation
    marks and spaces from the string to be analyzed. Moreover, it also
    removed numbers. However, the tests require numbers to be taken into
    account so change the code so that they are not removed.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,10 +3,11 @@ const palindromes = function (string) {
 
     // Gets array with only letters (based on the characters' ASCII codes)
     stringArray = stringArray.filter((character) => {
-        if (character.charCodeAt(0) < 97 || character.charCodeAt(0) > 122) {
-            return false;
-        } else {
+        let charCode = character.charCodeAt(0);
+        if ((charCode >= 48 && charCode <= 57) || (charCode >= 97 && charCode <= 122)) {
             return true;
+        } else {
+            return false;
         }
     });
 

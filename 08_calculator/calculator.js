ca2314ad364dc5feaf7020b763055226d7b45054
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-05-05 11:03:10 -0600

    Write code for 'sum' function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -4,11 +4,16 @@ const add = function(num1, num2) {
 
 const subtract = function(num1, num2) {
   return num1 - num2;
-	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arrayLength = array.length;
+	return array.reduce((total, item) => {
+    if (arrayLength === 1)
+      return item;
+    else
+      return total + item;
+  }, 0);
 };
 
 const multiply = function() {

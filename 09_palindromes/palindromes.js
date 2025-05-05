97c73984360036f4ea86545b5ecdd37e13ada975
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-05-05 13:59:51 -0600

    Write code to pass the 2nd test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,17 @@
 const palindromes = function (string) {
-    let reversedString = string.split("");
-    reversedString.reverse();
-    return reversedString.join("") === string;
+    let stringArray = string.toLowerCase().split("");
+
+    // Gets array with only letters (based on the characters' ASCII codes)
+    stringArray = stringArray.filter((character) => {
+        if (character.charCodeAt(0) < 97 || character.charCodeAt(0) > 122) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    let reversedString = stringArray.toReversed();
+    return reversedString.join("") === stringArray.join("");
 };
 
 // Do not edit below this line

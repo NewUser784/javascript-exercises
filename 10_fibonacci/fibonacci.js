e99a423ad20d21730a4b30a552279a8c9b28fafa
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-05-05 16:05:38 -0600

    Write code to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(inputNumber) {
+    let sum = 0;
+    let num1 = 1;
+    let num2 = 1;
+    inputNumber = +inputNumber;
+
+    if (inputNumber < 0) {
+        return "OOPS";
+    } else if (inputNumber === 0) {
+        return 0;
+    } else if (inputNumber === 1 || inputNumber === 2) {
+        return 1;
+    } else {
+        for (let i = 3; i <= inputNumber; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return sum;
+    }
 
 };
 

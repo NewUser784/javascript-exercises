b6d71b8fe769656b1345c722b578a045d6d715d2
Reader112 <184031753+NewUser784@users.noreply.github.com>
2025-05-06 10:36:39 -0600

    Write the code to pass the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let biggestAge = 0;
+    let oldestPersonObject;
 
+    for (let i = 0; i < people.length; i++) {
+        let person = people[i];
+        let currentAge;
+        if (person.yearOfDeath === undefined) {
+            currentAge = 2025 - person.yearOfBirth;
+        } else {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        
+        if (currentAge > biggestAge) {
+            biggestAge = currentAge;
+            oldestPersonObject = person;
+        }
+    }
+
+    return oldestPersonObject;
 };
 
 // Do not edit below this line
